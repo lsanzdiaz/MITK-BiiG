92615eb5e07a31c19812d867ca201e6ba4c38dfd
lsanz <lsanz@hggm.es>
2014-09-30 10:11:23 +0200

    Module ACVDMod and children directories and files added.

---
+++ Modules/ACVDMod/VolumeProcessing/VolumeAnisotropicDiffusion.cxx
@@ -0,0 +1,18 @@
+/*=========================================================================
+
+Program:   VolumeAnisotropicDiffusion : processes median filtering on input volume
+Module:    vtkSurface
+Language:  C++
+Date:      2011/09
+Auteur:   Sebastien Valette
+
+=========================================================================*/
+// .NAME VolumeMedian
+// .SECTION Description
+
+#include <vtkImageData.h>
+#include <vtkMetaImageReader.h>
+#include <vtkMetaImageWriter.h>
+#include <vtkImageAnisotropicDiffusion3D.h>
+
+
